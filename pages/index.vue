b460cb23f830e4e0a8022a1c404b7a60fb0989bc
CedricNtwari <cedric.ntwari@powercoders.org>
2021-11-18 21:56:19 +0100

    test primary

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
     <h1>Content 1</h1>
+    <Primary :title="title" :paragraphe="paragraphe" :button="button" />
   </div>
 </template>
+
+<script lang="ts">
+import Primary from '@/components/Primary.vue'
+
+export default ({
+  name: 'index',
+
+  components: {
+    Primary,
+  },
+  data() {
+    return {
+      title: 'Download Resume',
+      paragraphe: 'paragraphe',
+      button: 'button'
+    }
+  },
+})
+</script>
