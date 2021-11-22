249bc92556de86f587fd733e993be0aebd3d2ca3
CedricNtwari <cedric.ntwari@powercoders.org>
2021-11-22 18:43:52 +0100

    Revert "remove uncessary files"
    This reverts commit a9fdb339c2f53745aaf03e3253b5550b774765f3.

--- test/Primary.spec.ts
+++ test/Primary.spec.ts
@@ -1,6 +1,8 @@
 import { mount } from '@vue/test-utils'
 import Primary from '@/components/Primary.vue'
 import { Wrapper } from '@vue/test-utils'
+import Vue from 'vue'
+import { shallowMount } from '@/test/factory'
 
 describe('Primary', () => {
   it('renders a div', () => {
